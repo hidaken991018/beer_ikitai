b81d2435ac61e35e7ce2bb0fb7bef3c47b1bdf3f
hidaken991018 <ed.hidaken@gmail.com>
2025-10-15 08:18:16 +0900

    refactor:サンプルデータ変更

--- back/init-db/02_sample_data.sql
+++ back/init-db/02_sample_data.sql
@@ -1,22 +1,11 @@
 -- サンプルデータ挿入
 
 -- サンプル醸造所データ
-INSERT INTO brewery (name, address, description, latitude, longitude) VALUES
-('東京クラフトブルワリー', '東京都渋谷区1-1-1', '渋谷にあるクラフトビール醸造所です。IPA と ステイアウトが自慢です。', 35.6762, 139.6503),
-('横浜ベイブルワリー', '神奈川県横浜市中区2-2-2', '横浜港を望む醸造所。ピルスナーとヴァイツェンが人気です。', 35.4437, 139.6380),
-('大阪クラフトハウス', '大阪府大阪市北区3-3-3', '大阪の老舗醸造所。関西風の味わい深いビールを提供しています。', 34.7024, 135.4937),
-('福岡ホップファーム', '福岡県福岡市博多区4-4-4', '九州産ホップを使用したオリジナルビールが自慢の醸造所です。', 33.5904, 130.4017),
-('札幌ビアワークス', '北海道札幌市中央区5-5-5', '北海道の豊かな自然を活かしたクラフトビールを醸造しています。', 43.0642, 141.3469);
-
--- サンプルユーザープロファイル（テスト用）
-INSERT INTO user_profile (cognito_sub, display_name, icon_url) VALUES
-('demo-user-001', 'ビール太郎', 'https://example.com/icons/user1.png'),
-('demo-user-002', 'クラフト花子', 'https://example.com/icons/user2.png');
-
--- サンプル訪問データ
-INSERT INTO visit (user_profile_id, brewery_id, visited_at) VALUES
-(1, 1, NOW() - INTERVAL '7 days'),
-(1, 2, NOW() - INTERVAL '3 days'), 
-(1, 3, NOW() - INTERVAL '1 day'),
-(2, 1, NOW() - INTERVAL '5 days'),
-(2, 4, NOW() - INTERVAL '2 days');
+INSERT INTO brewery (name, address, description, latitude, longitude,created_at,updated_at) VALUES
+('伊勢角屋麦酒 神久工場', '伊勢市神久六丁目428番地', '三重県伊勢市にあるクラフトビール醸造所です。IPA が自慢です。', 34.49558856790372, 136.726220607552,CURRENT_TIMESTAMP,CURRENT_TIMESTAMP),
+('横浜ビール醸造所', '神奈川県横浜市中区住吉町6-68-1', '横浜港を望む醸造所。ピルスナーとヴァイツェンが人気です。', 35.44876257397394, 139.63406983775437,CURRENT_TIMESTAMP,CURRENT_TIMESTAMP),
+('BLACK TIDE BREWING', '宮城県気仙沼市南町3丁目2-5', '気仙沼の復旧・復興の一助になればという想い、そして「クラフトビールによるコミュニティーづくり」というキーワードのもと、どこからともなく僕たちは集まってきました。', 38.90514390741131, 141.57451548026768,CURRENT_TIMESTAMP,CURRENT_TIMESTAMP),
+('箕面ビール', '大阪府箕面市牧落3-19-11', '箕面の地で生まれた、人の手によって大切に作られるローカルビールです。', 34.82344235235449, 135.47264223006584,CURRENT_TIMESTAMP,CURRENT_TIMESTAMP),
+('DD4D BREWING', '愛媛県松山市千舟町4-2-6', 'これまで300種類以上のビールを全国、海外に販売。スポーツ、アパレル、製菓、映画など業界を越えたコラボも多数', 33.83679664169755, 132.766265910689,CURRENT_TIMESTAMP,CURRENT_TIMESTAMP),
+('蒲田温泉 BREWING', '東京都大田区蒲田本町2丁目23-2', '蒲田温泉だよ', 35.55622138589161, 139.71810504812046,CURRENT_TIMESTAMP,CURRENT_TIMESTAMP),
+('Hida BREWING', '東京都大田区蒲田本町1丁目8-5', '拠点', 35.556832371148566, 139.71602365396922,CURRENT_TIMESTAMP,CURRENT_TIMESTAMP);
