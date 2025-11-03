492b60e9c090356f5696ad76436dd8253512955a
hidaken991018 <ed.hidaken@gmail.com>
2025-11-03 11:17:26 +0900

    fix:

--- back/controllers/visit_controller.go
+++ back/controllers/visit_controller.go
@@ -49,9 +49,10 @@ func (c *VisitController) CheckIn() {
 		return
 	}
 
-	int_cognitoSub, err := strconv.Atoi(cognitoSub)
+	// ユーザープロファイル取得
+	userProfile, err := c.UserProfileUsecase.GetProfile(cognitoSub)
 	if err != nil {
-		c.ErrorResponse(400, "Invalid user profile cognito sub", "INVALID_COGNITO_SUB")
+		c.ErrorResponse(400, "User profile not found", "PROFILE_NOT_FOUND")
 		return
 	}
 
@@ -68,7 +69,7 @@ func (c *VisitController) CheckIn() {
 	}
 
 	visit, err := c.VisitUsecase.CheckIn(
-		int_cognitoSub,
+		userProfile.ID(),
 		request.BreweryID,
 		request.Latitude,
 		request.Longitude,
